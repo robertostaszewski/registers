5e5537382570f39dc45c208383f1f6b05291e187
robert <ostaszewski.robert@hotmail.com>
2021-03-18 22:16:58 +0100

    initial commit

---
+++ src/main/java/com/assignment/registers/RegistersApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.registers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistersApplication.class, args);
+	}
+
+}
